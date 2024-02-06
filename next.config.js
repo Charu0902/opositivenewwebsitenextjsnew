0ef2bd32fe76551c4f410e67527c853fdcb54f79
Charu0902 <charu.g@obbserv.com>
2024-02-06 18:20:38 +0530

    yguhj

---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    images: {
+      reactStrictMode: true,
+
+        //enter the domain or subdomain where you have WordPress installed 
+      domains: ['opositive.io'],
+    }
+}
